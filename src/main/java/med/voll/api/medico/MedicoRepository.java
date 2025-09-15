ca46a518bfa72e00927f7ca3381b5ba9bbb01749
Kauan Kelvin <kelvinkauan722@gmail.com>
2025-09-14 21:26:54 -0300

    feat: adicionar endpoints de atualização e exclusão lógica com @PutMapping e @DeleteMapping feat: implementar atualização de registros e exclusão lógica com @PathVariable feat: suportar update e exclusão lógica via parâmetros de URL feat: mapear @PutMapping e @DeleteMapping com exclusão lógica feat: atualizar e excluir registros logicamente usando path variables feat: adição da V3 do schema para atualizar dados do médico no banco de dados.

--- src/main/java/med/voll/api/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -1,6 +1,14 @@
 package med.voll.api.medico;
 
+import io.micrometer.observation.ObservationFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    List<Medico> ativo(Boolean ativo);
+
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 }
